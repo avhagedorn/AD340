eab878395a0f116bb699236c4a02534a075b54dd
avhagedorn <avhagedorn@gmail.com>
2021-07-05 19:15:46 -0500

    Added zipcode entry prompt.

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ssnEditText: EditText = findViewById<EditText>(R.id.etSSN)
+        val ssnSubmit: Button = findViewById<Button>(R.id.btnSubmitSSN)
+
+        ssnSubmit.setOnClickListener {
+            val ssn: String = ssnEditText.text.toString()
+            if (ssn.length == 5)
+                Toast.makeText(this, ssn, Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this, "Invalid zipcode!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onStart() {
